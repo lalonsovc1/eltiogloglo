4e3b9ccc4e461550218f20ea5183b41e040bef71
lalonsovc1 <lux5416@hotmail.com>
2026-06-13 04:07:50 -0500

    V1.13.06.26 Eltiogloglo

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://lalonsovc1.github.io',
+  base: 'eltiogloglo',
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
